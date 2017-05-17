191441fde6230cc49c5cd7dc4e57c2247578ccb4
JF <JF>
2017-05-17 16:55:58 -0600

    Arreglo del algoritmo

--- bakery.py
+++ bakery.py
@@ -1,9 +1,10 @@
 import _thread
 import time
 DELAY=1 #tiempo que espera en cada seccion
-NT=25 #numero de threads
+NT=25  #numero de threads
 choosing=[False]*NT
 number=[0]*NT
+critical=[False]*NT #se usa para monitorear que threads estan en la seccion critica en un momento dado
 #retorna el numero maximo en el arreglo de numeros
 def max_number():
     global choosing
@@ -14,12 +15,10 @@ def max_number():
             max=i
     return max
 #hace la comparacion de tuplas
-def comparacion_tuplas(j,i):
-    global choosing
-    global number
-    if(number[j]<number[i]):
+def comparacion_tuplas(a,b,c,d):
+    if(a<c):
         return True
-    elif (number[j]==number[i] and j<i):
+    elif (a==c and b<d):
         return True
     return False
 def lock(i):
@@ -31,7 +30,7 @@ def lock(i):
     for j in range(NT):
         while(choosing[j]):
             continue
-        while(number[j]!=0 and comparacion_tuplas(j,1)):
+        while(number[j]!=0 and comparacion_tuplas(number[j],j,number[i],i)):
             continue
 def unlock(i):
     global choosing
@@ -39,15 +38,33 @@ def unlock(i):
     number[i]=0
 def ejecutar_thread(i):
     while(True):
+        #entra a la seccion critica
         lock(i)
-        print("Thread "+str(i)+" en seccion critica\n")
+        #print("Thread "+str(i)+" en seccion critica\n")
+        critical[i]=True
         time.sleep(DELAY)
+        critical[i]=False
+        #sale de la seccion critica
         unlock(i)
-        print("Thread "+str(i)+" fuera de seccion critica\n")
+        #print("Thread "+str(i)+" fuera de seccion critica\n")
         time.sleep(DELAY)
+#monitorea en que seccion estan los threads
+#True: en la seccion critica
+#False: fuera de ella
+def monitorear():
+    tiempo=1
+    while(True):
+        print ("Tiempo: "+str(tiempo))
+        #solo puede haber un True en el vector que se imprime
+        #porque solo hay un thread en la seccion critica a la vez
+        print(critical)
+        tiempo+=1
+        time.sleep(1)
 def bakery():
     for i in range(NT):
         _thread.start_new_thread( ejecutar_thread, (i,) )
 bakery()
+monitorear()
+
     
     
